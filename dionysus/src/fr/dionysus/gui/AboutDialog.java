433220c766471b6eefa283eff47984368ec3b8bb
podgy_piglet <podgy_piglet@4f5da7a9-de5e-4495-83e2-abee53d8fe7e>
2013-05-11 16:48:58 +0000

    Finished a first version of About box

--- dionysus/src/fr/dionysus/gui/AboutDialog.java
+++ dionysus/src/fr/dionysus/gui/AboutDialog.java
@@ -16,6 +16,7 @@
 */
 package fr.dionysus.gui;
 
+import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JButton;
 import java.awt.GridBagLayout;
@@ -28,6 +29,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.SwingConstants;
+import java.awt.Component;
+import javax.swing.Box;
 
 /**
  * The dialog box for properties and proper credits
@@ -39,7 +42,7 @@ public class AboutDialog extends JDialog {
 		setTitle("About");
 		setResizable(false);
 		setModal(true);
-		setSize(350,400);
+		setSize(400,420);
 		setLocationRelativeTo(null);
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{444, 0};
@@ -57,19 +60,19 @@ public class AboutDialog extends JDialog {
 		getContentPane().add(panel, gbc_panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[]{0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
-		JLabel lblLogo = new JLabel("icon");
+		JLabel lblLogo = new JLabel("");
 		GridBagConstraints gbc_lblLogo = new GridBagConstraints();
 		gbc_lblLogo.insets = new Insets(0, 0, 5, 5);
 		gbc_lblLogo.gridx = 0;
 		gbc_lblLogo.gridy = 0;
 		panel.add(lblLogo, gbc_lblLogo);
 		
-		JLabel lblSoftwareName = new JLabel("Dionysus v");
+		JLabel lblSoftwareName = new JLabel(MainGUI2.SOFTWARE_NAME + " v" + MainGUI2.SOFTWARE_VERSION);
 		lblSoftwareName.setHorizontalAlignment(SwingConstants.LEFT);
 		lblSoftwareName.setFont(new Font("Tahoma", Font.BOLD, 20));
 		GridBagConstraints gbc_lblSoftwareName = new GridBagConstraints();
@@ -87,7 +90,7 @@ public class AboutDialog extends JDialog {
 		gbc_lblDesc.gridy = 1;
 		panel.add(lblDesc, gbc_lblDesc);
 		
-		JLabel lblCopyright = new JLabel("Copyright 2011-2013 podgy_piglet");
+		JLabel lblCopyright = new JLabel("(C) 2011-2013 podgy_piglet");
 		lblCopyright.setHorizontalAlignment(SwingConstants.LEFT);
 		GridBagConstraints gbc_lblCopyright = new GridBagConstraints();
 		gbc_lblCopyright.insets = new Insets(0, 0, 5, 0);
@@ -96,13 +99,32 @@ public class AboutDialog extends JDialog {
 		gbc_lblCopyright.gridy = 2;
 		panel.add(lblCopyright, gbc_lblCopyright);
 		
-		JLabel lblLicenseInfo = new JLabel("license goes here");
+		JLabel lblIconCopyright = new JLabel("Icons (C) 2007-2013 the GTK+ project, licensed under GPLv2");
+		lblIconCopyright.setHorizontalAlignment(SwingConstants.LEFT);
+		GridBagConstraints gbc_lblIconCopyright = new GridBagConstraints();
+		gbc_lblIconCopyright.anchor = GridBagConstraints.WEST;
+		gbc_lblIconCopyright.insets = new Insets(0, 0, 5, 0);
+		gbc_lblIconCopyright.gridx = 1;
+		gbc_lblIconCopyright.gridy = 3;
+		panel.add(lblIconCopyright, gbc_lblIconCopyright);
+		
+		Component verticalStrut = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
+		gbc_verticalStrut.insets = new Insets(0, 0, 5, 0);
+		gbc_verticalStrut.gridx = 1;
+		gbc_verticalStrut.gridy = 4;
+		panel.add(verticalStrut, gbc_verticalStrut);
+		
+		JLabel lblLicenseInfo = new JLabel("<html>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or(at your option) any later version.<br/><br/>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.<br/><br/>You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.</html>");
 		GridBagConstraints gbc_lblLicenseInfo = new GridBagConstraints();
+		gbc_lblLicenseInfo.fill = GridBagConstraints.HORIZONTAL;
 		gbc_lblLicenseInfo.gridx = 1;
-		gbc_lblLicenseInfo.gridy = 4;
+		gbc_lblLicenseInfo.gridy = 5;
 		panel.add(lblLicenseInfo, gbc_lblLicenseInfo);
 		
-		JButton btnOK = new JButton("OK");
+		ImageIcon ok = new ImageIcon("images/gtk-ok.png");
+		
+		JButton btnOK = new JButton("OK",ok);
 		GridBagConstraints gbc_btnOK = new GridBagConstraints();
 		gbc_btnOK.anchor = GridBagConstraints.NORTH;
 		gbc_btnOK.gridx = 0;
