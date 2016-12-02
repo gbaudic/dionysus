6847f3251719d286a7df8def776d2403d4e27e99
gbaudic <gbaudic@users.noreply.github.com>
2016-12-02 10:28:58 +0100

    Missing import in NewArticleDialog

--- src/net/sourceforge/dionysus/gui/NewArticleDialog.java
+++ src/net/sourceforge/dionysus/gui/NewArticleDialog.java
@@ -21,6 +21,7 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
