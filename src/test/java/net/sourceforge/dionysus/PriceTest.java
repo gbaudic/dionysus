4025748b8e502d26e5ac500723bd3d8e450ced61
Gwilherm Baudic <gbaudic@users.noreply.github.com>
2019-09-02 23:04:04 +0200

    First test!

---
+++ src/test/java/net/sourceforge/dionysus/PriceTest.java
@@ -0,0 +1,39 @@
+/* 	Dionysus, a student pub management software in Java
+    Copyright (C) 2011,2013,2015-2019  G. Baudic
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+package net.sourceforge.dionysus;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PriceTest {
+
+	/**
+	 * Test method for {@link net.sourceforge.dionysus.Price#getPrice()}.
+	 */
+	@Test
+	public void testGetPrice() {
+		Price fisher = new Price(1000);
+		Assert.assertEquals(fisher.getPrice(), 10.00, 0.001);
+	}
+	
+	@Test
+	public void testGetPriceWithDouble() {
+		Price fisher = new Price(12.45);
+		Assert.assertEquals(fisher.getPrice(), 12.45, 0.001);
+	}
+
+}
